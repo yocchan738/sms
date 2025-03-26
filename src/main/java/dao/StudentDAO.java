57449500ff4ab0c546e62a7d8055f7e6677cc74f
yocchan738 <soramame111.8.92@gmail.com>
2025-03-26 14:32:03 +0900

    daodao

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -10,6 +10,12 @@ import bean.Bean;
 
 public class StudentDAO extends DAO {
 
+	/**
+	 * 成績表を学生名で検索するメソッド
+	 * 
+	 * @param keyword 検索キーワード（部分一致）
+	 * @return List<Bean> 検索結果のリスト
+	 */
 	public List<Bean> searchScores(String keyword) throws Exception{
 		List<Bean> list=new ArrayList<>();
 		
@@ -44,6 +50,92 @@ public class StudentDAO extends DAO {
 		
 		return list;
 	}
+	/**
+	 * 成績表を学生IDで検索するメソッド
+	 * 
+	 * @param keyword 検索キーワード（完全一致）
+	 * @return List<Bean> 検索結果のリスト
+	 */
+	public List<Bean> searchScoresStudent(int keyword) throws Exception{
+		List<Bean> list=new ArrayList<>();
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"SELECT * FROM scores sc JOIN student s ON sc.student_id = s.student_id JOIN class c ON s.class_id = c.class_id JOIN school sch ON s.school_id = sch.school_id JOIN subjects sub ON sc.subject_id = sub.subject_id JOIN teacher t ON sub.teacher_id = t.teacher_id where s.student_id=?");
+		
+		st.setInt(1, keyword);
+		
+		ResultSet rs=st.executeQuery();
+		
+		while (rs.next()) {
+			Bean s=new Bean();
+			s.setClass_id(rs.getInt("class_id"));
+			s.setClass_name(rs.getString("class_name"));
+			s.setSchool_id(rs.getInt("school_id"));
+			s.setSchool_name(rs.getString("school_name"));
+			s.setStudent_id(rs.getInt("student_id"));
+	        s.setStudent_name(rs.getString("student_name"));
+	        s.setSubject_id(rs.getInt("subject_id"));
+	        s.setSubject_name(rs.getString("subject_name"));
+	        s.setTeacher_id(rs.getInt("teacher_id"));
+	        s.setTeacher_name(rs.getString("teacher_name"));
+	        s.setScore_id(rs.getInt("score_id"));
+	        s.setScore_value(rs.getInt("score_value"));
+	        list.add(s);
+		}
+		
+		st.close();
+		con.close();
+		
+		return list;
+	}
+	/**
+	 * 成績表をsubject_idで検索するメソッド
+	 * 
+	 * @param keyword 検索キーワード（完全一致）
+	 * @return List<Bean> 検索結果のリスト
+	 */
+	public List<Bean> searchScoresSubject(int keyword) throws Exception{
+		List<Bean> list=new ArrayList<>();
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"SELECT * FROM scores sc JOIN student s ON sc.student_id = s.student_id JOIN class c ON s.class_id = c.class_id JOIN school sch ON s.school_id = sch.school_id JOIN subjects sub ON sc.subject_id = sub.subject_id JOIN teacher t ON sub.teacher_id = t.teacher_id where sub.subject_id=?");
+		
+		st.setInt(1, keyword);
+		
+		ResultSet rs=st.executeQuery();
+		
+		while (rs.next()) {
+			Bean s=new Bean();
+			s.setClass_id(rs.getInt("class_id"));
+			s.setClass_name(rs.getString("class_name"));
+			s.setSchool_id(rs.getInt("school_id"));
+			s.setSchool_name(rs.getString("school_name"));
+			s.setStudent_id(rs.getInt("student_id"));
+	        s.setStudent_name(rs.getString("student_name"));
+	        s.setSubject_id(rs.getInt("subject_id"));
+	        s.setSubject_name(rs.getString("subject_name"));
+	        s.setTeacher_id(rs.getInt("teacher_id"));
+	        s.setTeacher_name(rs.getString("teacher_name"));
+	        s.setScore_id(rs.getInt("score_id"));
+	        s.setScore_value(rs.getInt("score_value"));
+	        list.add(s);
+		}
+		
+		st.close();
+		con.close();
+		
+		return list;
+	}
+	/**
+	 * 学生表を学生名で検索するメソッド
+	 * 
+	 * @param keyword 検索キーワード（部分一致）
+	 * @return List<Bean> 検索結果のリスト
+	 */
 	public List<Bean> searchStudent(String keyword) throws Exception{
 		List<Bean> list=new ArrayList<>();
 		
@@ -72,6 +164,11 @@ public class StudentDAO extends DAO {
 		
 		return list;
 	}
+	/**
+	 * クラス表を返すメソッド
+	 * 
+	 * @return List<Bean> 結果のリスト
+	 */
 	public List<Bean> searchClass() throws Exception{
 		List<Bean> list=new ArrayList<>();
 		
@@ -94,6 +191,11 @@ public class StudentDAO extends DAO {
 		
 		return list;
 	}
+	/**
+	 * 学校表を返すメソッド
+	 * 
+	 * @return List<Bean> 結果のリスト
+	 */
 	public List<Bean> searchSchool(String keyword) throws Exception{
 		List<Bean> list=new ArrayList<>();
 		
@@ -116,6 +218,11 @@ public class StudentDAO extends DAO {
 		
 		return list;
 	}
+	/**
+	 * 教科表を返すメソッド
+	 * 
+	 * @return List<Bean> 結果のリスト
+	 */
 	public List<Bean> searchSubject(String keyword) throws Exception{
 		List<Bean> list=new ArrayList<>();
 		
@@ -140,7 +247,12 @@ public class StudentDAO extends DAO {
 		
 		return list;
 	}
-	public List<Bean> searchTeacher(String keyword) throws Exception{
+	/**
+	 * 教諭表を返すメソッド
+	 * 
+	 * @return List<Bean> 結果のリスト
+	 */
+	public List<Bean> searchTeachers(String keyword) throws Exception{
 		List<Bean> list=new ArrayList<>();
 		
 		Connection con=getConnection();
@@ -163,6 +275,12 @@ public class StudentDAO extends DAO {
 		return list;
 	}
 	
+	/**
+     * クラス情報を追加するメソッド
+     * 
+     * @param Bean 追加するクラスオブジェクト
+     * @return int 追加された行数
+     */
 	public int insertClass(Bean c) throws Exception{
 		Connection con=getConnection();
 		
@@ -175,6 +293,12 @@ public class StudentDAO extends DAO {
 		con.close();
 		return line;
 	}
+	/**
+     * 学校情報を追加するメソッド
+     * 
+     * @param Bean 追加する学校オブジェクト
+     * @return int 追加された行数
+     */
 	public int insertSchool(Bean sch) throws Exception{
 		Connection con=getConnection();
 		
@@ -187,6 +311,12 @@ public class StudentDAO extends DAO {
 		con.close();
 		return line;
 	}
+	/**
+     * 学生情報を追加するメソッド
+     * 
+     * @param Bean 追加する学生オブジェクト
+     * @return int 追加された行数
+     */
 	public int insertStudent(Bean s) throws Exception{
 		Connection con=getConnection();
 		
@@ -201,7 +331,13 @@ public class StudentDAO extends DAO {
 		con.close();
 		return line;
 	}
-	public int insertTeacher(Bean t) throws Exception{
+	/**
+     * 教諭情報を追加するメソッド
+     * 
+     * @param Bean 追加する教諭オブジェクト
+     * @return int 追加された行数
+     */
+	public int insertTeachers(Bean t) throws Exception{
 		Connection con=getConnection();
 		
 		PreparedStatement st=con.prepareStatement(
@@ -213,6 +349,12 @@ public class StudentDAO extends DAO {
 		con.close();
 		return line;
 	}
+	/**
+     * 教科情報を追加するメソッド
+     * 
+     * @param Bean 追加する教科オブジェクト
+     * @return int 追加された行数
+     */
 	public int insertSubject(Bean sub) throws Exception{
 		Connection con=getConnection();
 		
@@ -226,6 +368,12 @@ public class StudentDAO extends DAO {
 		con.close();
 		return line;
 	}
+	/**
+     * 成績情報を追加するメソッド
+     * 
+     * @param Bean 追加する成績オブジェクト
+     * @return int 追加された行数
+     */
 	public int insertScores(Bean sc) throws Exception{
 		Connection con=getConnection();
 		
@@ -234,6 +382,85 @@ public class StudentDAO extends DAO {
 		st.setInt(1, sc.getStudent_id());
 		st.setInt(2, sc.getSubject_id());
 		st.setInt(3, sc.getScore_value());
+		int line=st.executeUpdate();
+		
+		st.close();
+		con.close();
+		return line;
+	}
+	
+	/**
+     * クラス情報を更新するメソッド
+     * 
+     * @param Bean 更新するクラスオブジェクト
+     * @return int 更新された行数
+     */
+	public int updateClass(Bean c) throws Exception{
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"update class set class_name=? where class_id=?");
+		st.setString(1, c.getClass_name());
+		st.setInt(2, c.getClass_id());
+		int line=st.executeUpdate();
+		
+		st.close();
+		con.close();
+		return line;
+	}
+	/**
+     * 学校情報を更新するメソッド
+     * 
+     * @param Bean 更新する学校オブジェクト
+     * @return int 更新された行数
+     */
+	public int updateSchool(Bean sch) throws Exception{
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"update school set school_name=? where school_id=?");
+		st.setString(1, sch.getSchool_name());
+		st.setInt(2, sch.getSchool_id());
+		int line=st.executeUpdate();
+		
+		st.close();
+		con.close();
+		return line;
+	}
+	/**
+     * 学生情報を更新するメソッド
+     * 
+     * @param Bean 更新する学生オブジェクト
+     * @return int 更新された行数
+     */
+	public int updateStudent(Bean s) throws Exception{
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"update student set student_name=?, class_id=?, school_id=? where class_id=?");
+		st.setString(1, s.getStudent_name());
+		st.setInt(2, s.getClass_id());
+		st.setInt(3, s.getSchool_id());
+		st.setInt(4, s.getStudent_id());
+		int line=st.executeUpdate();
+		
+		st.close();
+		con.close();
+		return line;
+	}
+	/**
+     * 教諭情報を更新するメソッド
+     * 
+     * @param Bean 更新する教諭オブジェクト
+     * @return int 更新された行数
+     */
+	public int updateTeachers(Bean t) throws Exception{
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"update teachers set teacher_name=? where teacher_id=?");
+		st.setString(1, t.getTeacher_name());
+		st.setInt(2, t.getSubject_id());
 		int line=st.executeUpdate();
 		
 		st.close();
