5fae533767dd57bd7202feba4c938eb8816fd008
yocchan738 <soramame111.8.92@gmail.com>
2025-03-27 12:16:27 +0900

    dao 修正

---
+++ src/main/java/score_manegement/ScoreInsertCommit.java
@@ -0,0 +1,47 @@
+package score_manegement;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Bean;
+import dao.StudentDAO;
+
+@WebServlet(urlPatterns = {"/scoremanegement/insert/commit"})
+public class ScoreInsertCommit extends HttpServlet {
+
+	public void doPost(
+	        HttpServletRequest request, HttpServletResponse response
+	    ) throws ServletException, IOException {
+	        
+	        // エラーハンドリング
+	        try {
+	            // フォームから送信されたパラメータを取得
+	            int student_id = Integer.parseInt(request.getParameter("student_id"));
+	            int subject_id = Integer.parseInt(request.getParameter("subject_id"));
+	            int score_value = Integer.parseInt(request.getParameter("score_value"));
+
+	            // Studentオブジェクトの作成とデータ設定
+	            Bean s = new Bean();
+	            s.setStudent_id(student_id);
+	            s.setSubject_id(subject_id);
+	            s.setScore_value(score_value);
+
+	            // DAOを利用してデータベースに学生情報を追加
+	            StudentDAO dao = new StudentDAO();
+	            int line = dao.insertScores(s);
+	            
+	            // 挿入成功の場合、成功画面に遷移
+	            if (line > 0) {
+	                request.getRequestDispatcher("/score_manegement/score_insert_commit.jsp").forward(request, response);
+	            }
+	        } catch (Exception e) {
+	            // 例外が発生した場合、失敗画面に遷移
+	            request.getRequestDispatcher("/insert/failure.jsp").forward(request, response);
+	        }
+	    }
+}
