277eebd94727086a7fc18c17658dd6a27f4977b3
yocchan738 <soramame111.8.92@gmail.com>
2025-03-28 10:52:19 +0900

    score_update

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -62,7 +62,7 @@ public class StudentDAO extends DAO {
 		Connection con=getConnection();
 		
 		PreparedStatement st=con.prepareStatement(
-				"SELECT * FROM scores sc JOIN student s ON sc.student_id = s.student_id JOIN class c ON s.class_id = c.class_id JOIN school sch ON s.school_id = sch.school_id JOIN subjects sub ON sc.subject_id = sub.subject_id JOIN teacher t ON sub.teacher_id = t.teacher_id where sc,score_id=?");
+				"SELECT * FROM scores sc JOIN student s ON sc.student_id = s.student_id JOIN class c ON s.class_id = c.class_id JOIN school sch ON s.school_id = sch.school_id JOIN subjects sub ON sc.subject_id = sub.subject_id JOIN teacher t ON sub.teacher_id = t.teacher_id where sc.score_id=?");
 		
 		st.setInt(1, keyword);
 		
@@ -216,6 +216,40 @@ public class StudentDAO extends DAO {
 				"SELECT * FROM student s join class c on s.class_id=c.class_id join school sch on s.school_id=sch.school_id where s.student_name like ?");
 		
 		st.setString(1, "%"+keyword+"%");
+		
+		ResultSet rs=st.executeQuery();
+		
+		while (rs.next()) {
+			Bean s=new Bean();
+			s.setClass_id(rs.getInt("class_id"));
+			s.setClass_name(rs.getString("class_name"));
+			s.setSchool_id(rs.getInt("school_id"));
+			s.setSchool_name(rs.getString("school_name"));
+			s.setStudent_id(rs.getInt("student_id"));
+	        s.setStudent_name(rs.getString("student_name"));
+	        list.add(s);
+		}
+		
+		st.close();
+		con.close();
+		
+		return list;
+	}
+	/**
+	 * 学生表を学生IDで検索するメソッド
+	 * 
+	 * @param keyword 検索キーワード（完全一致）
+	 * @return List<Bean> 検索結果のリスト
+	 */
+	public List<Bean> searchStudentID(int keyword) throws Exception{
+		List<Bean> list=new ArrayList<>();
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st=con.prepareStatement(
+				"SELECT * FROM student s join class c on s.class_id=c.class_id join school sch on s.school_id=sch.school_id where s.student_id = ?");
+		
+		st.setInt(1, keyword);
 		
 		ResultSet rs=st.executeQuery();
 		
--- src/main/java/score_manegement/ScoreUpdate.java
+++ src/main/java/score_manegement/ScoreUpdate.java
@@ -24,21 +24,44 @@ public class ScoreUpdate extends HttpServlet {
         
         try {
         	int score_id = Integer.parseInt(request.getParameter("score_id"));
+        	int student_id = 0;
+        	int subject_id = 0;
             // StudentDAOのインスタンス生成
             StudentDAO dao = new StudentDAO();
             
             // 成績情報の全件取得（検索キーワードに空文字を指定）
             List<Bean> list_scores = dao.searchScores("");
             
-            // リクエストパラメータ "score_id" に基づき、該当する学生情報を取得
+            // 学生情報の全件取得（検索キーワードに空文字を指定）
+            List<Bean> list_student = dao.searchStudent("");
+            
+            // 科目情報の全件取得（検索キーワードに空文字を指定）
+            List<Bean> list_subject = dao.searchSubject("");
+            
+            // "score_id" に基づき、該当する成績情報を取得
             List<Bean> select_scores = dao.searchScoresID(score_id);
+            // 成績情報からそれぞれのIDを取得
+            for (Bean b : select_scores) {
+            	student_id = b.getStudent_id();
+            	subject_id = b.getSubject_id();
+            }
+            
+            // "student_id" に基づき、該当する学生情報を取得
+            List<Bean> select_student = dao.searchStudentID(student_id);
+            
+            // "subject_id" に基づき、該当する学生情報を取得
+            List<Bean> select_subject = dao.searchSubjectID(subject_id);
             
             // 取得したデータをリクエスト属性に設定
-            request.setAttribute("list", list_scores);
-            request.setAttribute("list_s", select_scores);
+            request.setAttribute("list_scores", list_scores);
+            request.setAttribute("select_scores", select_scores);
+            request.setAttribute("list_student", list_student);
+            request.setAttribute("select_student", select_student);
+            request.setAttribute("list_subject", list_subject);
+            request.setAttribute("select_subject", select_subject);
             
-            // update/input.jsp にフォワードして画面表示
-            request.getRequestDispatcher("/update/input.jsp").forward(request, response);
+            // score_manegement/score_update.jsp にフォワードして画面表示
+            request.getRequestDispatcher("/score_manegement/score_update.jsp").forward(request, response);
         } catch (Exception e) {
             // 例外発生時、エラーメッセージのスタックトレースを出力
             e.printStackTrace(out);
