4ab2b4faa1a358436a06fe089421f06409184959
yocchan738 <soramame111.8.92@gmail.com>
2025-04-10 10:20:40 +0900

    admin -> authentication

---
+++ src/main/java/bean/Authentication.java
@@ -0,0 +1,28 @@
+package bean;
+
+public class Authentication implements java.io.Serializable {
+	
+	private int user_id;
+	private String login_id;
+	private String password;
+	
+	public int getId() {
+		return user_id;
+	}
+	public String getLoginId() {
+		return login_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setId(int user_id) {
+		this.user_id=user_id;
+	}
+	public void setLogin(String login_id) {
+		this.login_id=login_id;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
+}
---
+++ src/main/java/dao/AuthenticationDAO.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.Authentication;
+
+public class AuthenticationDAO extends DAO {
+	public Authentication search(String login_id, String password)
+		throws Exception {
+		Authentication authentication=null;
+		
+		Connection con=getConnection();
+		
+		PreparedStatement st;
+		st=con.prepareStatement("select * from users where login_id=? and password=?");
+		st.setString(1, login_id);
+		st.setString(2, password);
+		ResultSet rs=st.executeQuery();
+		
+		while (rs.next()) {
+			authentication=new Authentication();
+			authentication.setId(rs.getInt("id"));
+			authentication.setLogin(rs.getString("login_id"));
+			authentication.setPassword(rs.getString("password"));
+		}
+		
+		st.close();
+		con.close();
+		return authentication;
+	}
+}
